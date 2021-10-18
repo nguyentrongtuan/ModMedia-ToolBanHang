acc5af84a0b551c97e16d8c0847abcfdaa5bae40
nguyentrongtuan <nguyentrongtuan.st@gmail.com>
2021-10-18 16:05:25 +0700

    Tool ban hang MVVM

---
+++ ToolBanHang/ToolBanHang/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ToolBanHang.ViewModel
+{
+    class MainViewModel: BaseViewModel
+    {
+        public bool IsLoaded { set; get; }
+        public MainViewModel()
+        {
+            
+            if (!IsLoaded)
+            {
+                IsLoaded = true;
+                // MessageBox.Show("a");
+            }
+        }
+    }
+}
